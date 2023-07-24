c0851668d99a35c824f89ae71658b663b29005fe
smiley-uriux <andrews@uriux.com>
2023-07-24 11:34:57 -0400

    fix: allow explicit any eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     rules: {
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        "@typescript-eslint/no-explicit-any": "off"
     },
     ignorePatterns: ['dist/*'],
 };
